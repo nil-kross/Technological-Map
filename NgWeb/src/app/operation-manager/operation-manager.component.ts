0581d8ca31037f8645db221f90f32a3575dbc03d
nil-kross <nil.kross@gmail.com>
2019-03-05 20:15:09 +0300

    Add operation manager component.

---
+++ NgWeb/src/app/operation-manager/operation-manager.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'operation-manager',
+  templateUrl: './operation-manager.component.html',
+  styleUrls: ['./operation-manager.component.css']
+})
+export class OperationManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSaveClick() {
+    ;
+  }
+
+}
