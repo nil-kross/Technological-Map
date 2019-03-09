15a1a35dee4f8a42c584cb37a406880eed521d58
nil-kross <nil.kross@gmail.com>
2019-03-09 22:00:47 +0300

    Implement operation manager component.

--- NgWeb/src/app/operation-manager/operation-manager.component.ts
+++ NgWeb/src/app/operation-manager/operation-manager.component.ts
@@ -1,4 +1,12 @@
+import { IEquipment } from './../../shared/IEquipment';
+import { controls } from './../../shared/Controls';
+import { equipements } from './../../shared/Equipment';
+import { instruments } from './../../shared/Instruments';
 import { Component, OnInit } from '@angular/core';
+import { operationGroups } from '../../shared/OperationGroups';
+import { IOperationGroup } from '../../shared/IOperationGroup';
+import { IInstrument } from '../../shared/IInstrument';
+import { IControl } from '../../shared/IControl';
 
 @Component({
   selector: 'operation-manager',
@@ -7,13 +15,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperationManagerComponent implements OnInit {
 
+  get operationGroupOptions(): IOperationGroup[] {
+    return operationGroups;
+  }
+
+  get instrumentOptions(): IInstrument[] {
+    return instruments;
+  }
+
+  get equipmentOptions(): IEquipment[] {
+    return equipements;
+  }
+
+  get controlOptions(): IControl[] {
+    return controls;
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onSaveClick() {
-    ;
+    // TODO
+  }
+
+  onDeleteClick() {
+    // TODO
+  }
+
+  onMoveUpClick() {
+    // TODO
+  }
+
+  onMoveDownClick() {
+    // TODO
   }
 
 }
