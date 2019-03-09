a79ab2ec62a952d7ffaf2776a2566f5078333db5
nil-kross <nil.kross@gmail.com>
2019-03-09 21:57:26 +0300

    Fix flyout component.

--- NgWeb/src/app/flyout/flyout.component.ts
+++ NgWeb/src/app/flyout/flyout.component.ts
@@ -13,12 +13,21 @@ enum FlyoutState {
 export class FlyoutComponent implements OnInit {
   state: FlyoutState = FlyoutState.Collapsed;
 
+  get isOpened(): boolean {
+    return this.state === FlyoutState.Expanded;
+  }
+
   constructor() { }
 
-  onClick() {
-    this.state = this.state === FlyoutState.Collapsed ? FlyoutState.Expanded : FlyoutState.Collapsed;
+  onClick(isOpened: boolean) {
+    this.setFlyoutState(isOpened);
+    console.log(this.state);
   }
 
   ngOnInit() {
+  }
+
+  private setFlyoutState(isOpened: boolean) {
+    this.state = isOpened ? FlyoutState.Expanded : FlyoutState.Collapsed;
   }
 }
