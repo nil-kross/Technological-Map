616a1c4edd20984852bbe946a8df185647d7c1f2
nil-kross <nil.kross@gmail.com>
2019-03-11 23:30:52 +0300

    Remake flyout.

--- NgWeb/src/app/flyout/flyout.component.ts
+++ NgWeb/src/app/flyout/flyout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 enum FlyoutState {
   Collapsed = 0,
@@ -19,9 +19,12 @@ export class FlyoutComponent implements OnInit {
 
   constructor() { }
 
+  close() {
+    this.setFlyoutState(false);
+  }
+
   onClick(isOpened: boolean) {
     this.setFlyoutState(isOpened);
-    console.log(this.state);
   }
 
   ngOnInit() {
